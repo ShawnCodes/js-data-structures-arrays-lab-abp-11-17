6fe477b46e0084b00faddef3e14279332bbe30e0
Shawn T <shawn@huthunt.com>
2017-11-24 05:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name){
-  drivers.splice(3, 0, "Broom");
+let appendDriver = drivers.splice(3, 0, "Broom");
   return drivers
 }
